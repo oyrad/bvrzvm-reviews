151e74d16c7e1a30d0d6c25f43bccbed5ce352d2
oyrad <dario.susanj2@gmail.com>
2023-02-05 15:03:14 +0100

    rating calculator fix

--- frontend/src/util/calculateRating.js
+++ frontend/src/util/calculateRating.js
@@ -2,9 +2,9 @@ export function calculateRating(reviews) {
   console.log(reviews);
   let rating = 0;
   if (reviews.length > 0) {
-    reviews.forEach((rev) => (rating = rating + parseInt(rev.rating)));
+    reviews.forEach((rev) => (rating = rating + parseFloat(rev.rating)));
     console.log(rating);
-    rating = rating / reviews.length;
+    rating = Math.round((rating / reviews.length) * 10) / 10;
 
     rating = `${rating}/10`;
   } else {
