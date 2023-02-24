38e87d918ff2b1a87a98a74b87d829bbb217d1c3
Dario <dario.susanj2@gmail.com>
2023-02-25 00:09:30 +0100

    cors fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
           Accept: "application/json",
           "Content-type": "application/json",
           "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin": process.env.REACT_APP_SERVER_URL,
         },
       })
         .then((res) => {
